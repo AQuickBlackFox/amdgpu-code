6d9ae833040de96c7a7963a671e5c0846f9b4881
Aditya Atluri <Aditya.Atluri@amd.com>
2016-11-24 22:44:33 -0600

    added sample for min/max

---
+++ sdwa/sdwa_comp.cpp
@@ -0,0 +1,65 @@
+#include<hip/hip_runtime.h>
+#include<hip/hip_runtime_api.h>
+#include<iostream>
+
+#define LEN 64
+#define SIZE LEN<<2
+
+#define fileName "sdwa_comp.co"
+#define kernelName "sdwa_comp"
+
+int main() {
+  unsigned *Ah, *Bh, *Ch;
+  hipDeviceptr_t Ad, Bd, Cd;
+  Ah = new unsigned[LEN];
+  Bh = new unsigned[LEN];
+  Ch = new unsigned[LEN];
+
+  for(unsigned i=0;i<LEN;i++) {
+    Ah[i] = 2;
+    Bh[i] = 3;
+    Ch[i] = 0;
+  }
+
+  hipInit(0);
+  hipDevice_t device;
+  hipCtx_t context;
+  hipDeviceGet(&device, 0);
+  hipCtxCreate(&context, 0, device);
+  hipModule_t Module;
+  hipFunction_t Function;
+
+  hipMalloc((void**)&Ad, SIZE);
+  hipMalloc((void**)&Bd, SIZE);
+  hipMalloc((void**)&Cd, SIZE);
+
+  hipMemcpyHtoD(Ad, Ah, SIZE);
+  hipMemcpyHtoD(Bd, Bh, SIZE);
+
+  hipModuleLoad(&Module, fileName);
+  hipModuleGetFunction(&Function, Module, kernelName);
+
+  struct {
+    void *Ad;
+    void *Bd;
+    void *Cd;
+  } args;
+
+  args.Ad = Ad;
+  args.Bd = Bd;
+  args.Cd = Cd;
+
+  size_t size = sizeof(args);
+
+  void *config[] = {
+    HIP_LAUNCH_PARAM_BUFFER_POINTER, &args,
+    HIP_LAUNCH_PARAM_BUFFER_SIZE, &size,
+    HIP_LAUNCH_PARAM_END
+  };
+
+  hipModuleLaunchKernel(Function, 1,1,1, LEN,1,1, 0, 0, NULL, (void**)&config);
+
+  hipMemcpyDtoH(Ch, Cd, SIZE);
+
+  std::cout<<Ch[10]<<std::endl;
+}
--- sdwa/sdwa_comp.ll
+++ sdwa/sdwa_comp.ll
@@ -1,9 +1,10 @@
+; using max and min with sdwa
 declare i32 @llvm.amdgcn.workitem.id.x()
 
 
 
-define i32 @sdwa(i32 %in1, i32 %in2) {
-  %val = tail call i32 asm "v_mul_u32_u24_sdwa $0, $1, $2 dst_sel:DWORD dst_unused:UNUSED_PAD src0_sel:DWORD src1_sel:DWORD","=v,v,v"(i32 %in1, i32 %in2)
+define i32 @sdwa_min_u16(i32 %in1, i32 %in2) {
+  %val = tail call i32 asm "v_min_u16_sdwa $0, $1, $2 dst_sel:WORD_0 dst_unused:UNUSED_PAD src0_sel:WORD_0 src1_sel:WORD_0","=v,v,v"(i32 %in1, i32 %in2)
   ret i32 %val
 }
 
@@ -14,7 +15,7 @@ define spir_kernel void @sdwa_mul(i32 addrspace(1)* %in1, i32 addrspace(1)* %in2
   %out_ptr = getelementptr i32, i32 addrspace(1)* %out, i32 %id
   %r1 = load i32, i32 addrspace(1)* %in1_ptr
   %r2 = load i32, i32 addrspace(1)* %in2_ptr
-  %r3 = call i32 @sdwa(i32 %r1, i32 %r2)
+  %r3 = call i32 @sdwa_min_u16(i32 %r1, i32 %r2)
   store i32 %r3, i32 addrspace(1)* %out_ptr
   ret void
 }
