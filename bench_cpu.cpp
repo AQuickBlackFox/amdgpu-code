1605db313a28f65fba0a545f63cb2e72c59aa74c
Aditya Atluri <adityaatluri@users.noreply.github.com>
2016-12-26 09:21:44 -0500

    Create bench_cpu.cpp

---
+++ bench_cpu.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<time.h>
+
+#define ITER 1024*1024
+#define WI 64
+#define USECPSEC 1000000ULL
+
+void Bench(float *In, float *Out){
+	for (int i = 0; i < ITER; i++){
+		for (int j = 0; j < WI; j++){
+			Out[j] = Out[j] + In[j];
+		}
+	}
+}
+
+int main() {
+	float *In = new float[WI];
+	float *Out = new float[WI];
+
+	clock_t start = clock();
+	Bench(In, Out);
+	clock_t stop = clock();
+	double t = (double)(stop - start) / (CLOCKS_PER_SEC);
+	std::cout << (WI*ITER)/(t*1000000)<< std::endl;
+
+
+	unsigned long long ops = ITER;
+	ops *= WI;
+
+}
