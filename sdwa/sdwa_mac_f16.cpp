dc24bc83e68e66828d4247803e3baa42b70cf66a
Aditya Atluri <adityaatluri@users.noreply.github.com>
2017-05-14 15:38:04 -0500

    Create sdwa_mac_f16.cpp

---
+++ sdwa/sdwa_mac_f16.cpp
@@ -0,0 +1,88 @@
+/*
+Copyright (c) 2016 Aditya Atluri. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include<hip/hip_runtime.h>
+#include<hip/hip_runtime_api.h>
+#include<iostream>
+
+#define LEN 64
+#define SIZE LEN<<2
+
+#define fileName "smacf16.co"
+#define kernelName "hello_world"
+
+#define HALF 0x3800
+#define HALF2 0x38003800
+#define ONE2 0x3C003C00
+
+int main() {
+  unsigned *Ah, *Bh, *Ch;
+  hipDeviceptr_t Ad, Bd, Cd;
+  Ah = new unsigned[LEN];
+  Bh = new unsigned[LEN];
+  Ch = new unsigned[LEN];
+
+  for(unsigned i=0;i<LEN;i++) {
+    Ah[i] = HALF2;
+    Bh[i] = ONE2;
+    Ch[i] = HALF2;
+  }
+
+  hipInit(0);
+  hipDevice_t device;
+  hipCtx_t context;
+  hipDeviceGet(&device, 0);
+  hipCtxCreate(&context, 0, device);
+  hipModule_t Module;
+  hipFunction_t Function;
+
+  hipMalloc((void**)&Ad, SIZE);
+  hipMalloc((void**)&Bd, SIZE);
+  hipMalloc((void**)&Cd, SIZE);
+
+  hipMemcpyHtoD(Ad, Ah, SIZE);
+  hipMemcpyHtoD(Bd, Bh, SIZE);
+  hipMemcpyHtoD(Cd, Ch, SIZE);
+  hipModuleLoad(&Module, fileName);
+  hipModuleGetFunction(&Function, Module, kernelName);
+
+  struct {
+    void *Ad;
+    void *Bd;
+    void *Cd;
+  } args;
+
+  args.Ad = Ad;
+  args.Bd = Bd;
+  args.Cd = Cd;
+
+  size_t size = sizeof(args);
+
+  void *config[] = {
+    HIP_LAUNCH_PARAM_BUFFER_POINTER, &args,
+    HIP_LAUNCH_PARAM_BUFFER_SIZE, &size,
+    HIP_LAUNCH_PARAM_END
+  };
+
+  hipModuleLaunchKernel(Function, 1,1,1, LEN,1,1, 0, 0, NULL, (void**)&config);
+
+  hipMemcpyDtoH(Ch, Cd, SIZE);
+
+  std::cout<<std::hex<<Ch[10]<<std::endl;
+}
