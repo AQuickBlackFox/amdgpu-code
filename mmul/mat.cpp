574c89ac29fdfa55d5ba1577e97923a652b7df55
Aditya Atluri <Aditya.Atluri@amd.com>
2017-06-04 23:19:02 -0500

    fixed half gemm partially

--- mmul/mat.cpp
+++ mmul/mat.cpp
@@ -28,30 +28,34 @@ __global__ void Dotv1(T* Y, T* W, T* X)
     int bx = hipBlockIdx_x;
     int by = hipBlockIdx_y;
 
-    int row = ty + by * FH_TILE_Y_32;
-    int col = tx + bx * FH_TILE_X_16;
+    int row = ty + by * 16;
+    int col = tx + bx * 16;
 
     __half2 *_W = (__half2*)W;
     __half2 *_X = (__half2*)X;
     __half2 *_Y = (__half2*)Y;
-    __shared__ __half2 sW[FH_TILE_Y_32][FH_TILE_X_16];
-    __shared__ __half2 sX[FH_TILE_Y_32][FH_TILE_X_16];
-    __half2 C = {0.0, 0.0};
-    for (int j = 0; j < w_width / FH_TILE_X_16; j++) {
-        sW[2*ty][tx] = _W[row*w_width/2 + (j*FH_TILE_X_16 + tx)];
-        sW[2*ty+1][tx] = _W[row*w_width/2 + (j*FH_TILE_X_16 + tx)];
-        sX[2*ty][tx] = _X[col + (j*FH_TILE_X_16+ty)*x_width];
-        sX[2*ty+1][tx] = _X[col + (2*(j*FH_TILE_X_16+ty)+1)*x_width];
+    __shared__ __half2 sW[32][16];
+    __shared__ __half2 sX[32][16];
+    __half2 c0 = {0.0, 0.0};
+    __half2 c1 = {0.0, 0.0};
+    for (int j = 0; j < w_width / 32; j++) {
+        sW[ty][tx] = _W[row*(32) + (j*16 + tx)];
+        sX[ty][tx] = _X[col + (j*16+ty)*32];
         __syncthreads();
+
         for (int i = 0; i < FH_TILE_Y_16; i++) {
-            __half2 a = sW[ty][i];
+            __half2 a0 = sW[2*ty][i];
+            __half2 a1 = sW[2*ty+1][i];
             __half2 b0 = sX[2*i][tx];
             __half2 b1 = sX[2*i+1][tx];
-            C.x = C.x + a.x * b0.x + a.y * b1.x;
-            C.y = C.y + a.x * b0.y + a.y * b1.y;
+            c0.x = c0.x + a0.x * b0.x + a0.y * b1.x;
+            c0.y = c0.y + a0.x * b0.y + a0.y * b1.y;
+            c1.x = c1.x + a1.x * b0.x + a1.y * b1.x;
+            c1.y = c1.y + a1.x * b0.y + a1.y * b1.y;
         }
         __syncthreads();
-        _Y[row*x_width/2+col] = C;
+
+        _Y[row*16+col] = c0;
     }
 }
 
@@ -66,13 +70,13 @@ int main()
     Yh = (half*)malloc(Y_SIZE);
 
     for(int i=0;i<W_HEIGHT*W_WIDTH;i++){
-        Wh[i] = 1.0;
+        Wh[i] = 0.5;
     }
     for(int i=0;i<X_HEIGHT*X_WIDTH;i++){
         Xh[i] = 1.0;
     }
     for(int i=0;i<Y_HEIGHT*Y_WIDTH;i++){
-        Yh[i] = 1.0;
+        Yh[i] = 2.0;
     }
 
     hipMalloc(&Wd, W_SIZE);
@@ -83,8 +87,8 @@ int main()
     hipMemcpy(Xd, Xh, X_SIZE, hipMemcpyHostToDevice);
     hipMemcpy(Yd, Yh, Y_SIZE, hipMemcpyHostToDevice);
 
-    dim3 dimBlock(FH_TILE_X_16, FH_TILE_Y_32);
-    dim3 dimGrid(X_WIDTH/dimBlock.x, W_HEIGHT/dimBlock.y);
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid(X_WIDTH/dimBlock.x/2, W_HEIGHT/dimBlock.y/2);
     hipLaunchKernelGGL((Dotv1<half, W_HEIGHT, W_WIDTH, X_HEIGHT, X_WIDTH>), dimGrid, dimBlock, 0, 0, Yd, Wd, Xd);
     hipDeviceSynchronize();
     hipMemcpy(Yh, Yd, Y_SIZE, hipMemcpyDeviceToHost);
